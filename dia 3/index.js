923c9eb234874c7bb9f7ee167f091b98a82f6122
GabrielMiranda7 <gabrielmirandamateus@gmail.com>
2023-09-19 23:32:52 -0300

    criando calculadora de imc com as respostas do usuário

---
+++ dia 3/index.js
@@ -0,0 +1,12 @@
+let nome = prompt('digite o seu nome: ')
+
+let idade =  prompt('digite a sua idade: ')
+
+let anoDeNascimento = 2023 - idade
+
+let altura = parseFloat(prompt('digite a sua altura:'))
+
+let peso = parseInt(prompt("digite o seu peso: "))
+
+let imc = peso/(altura * altura)
+onsole.log("Olá " + nome + ", você tem " + idade + " anos, nasceu em " + anoDeNascimento + ", tem " + altura + " de altura, pesa " + peso + "kg, seu IMC é " + imc + " Kg/m2")
